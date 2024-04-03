e27c471a2a3d89cc007afca375f8e86617322cef
maksimyurau <2004softwaredeveloper@gmail.com>
2024-04-03 18:20:40 +0300

    Updated project. Updated NoteColor.kt. Updated fun NoteColor().

--- app/src/main/java/com/maksimyurau/android/lab5_notes/ui/components/NoteColor.kt
+++ app/src/main/java/com/maksimyurau/android/lab5_notes/ui/components/NoteColor.kt
@@ -3,8 +3,10 @@ package com.maksimyurau.android.lab5_notes.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -15,6 +17,7 @@ fun NoteColor() {
         modifier = Modifier
             .size(40.dp)
             .background(Color.Red)
+            .clip(CircleShape)
     )
 }
 
