d8610f043dd927fa58eb7a85d8d32b4db7b7f5f6
MaksimYurau <2004softwaredeveloper@gmail.com>
2024-04-13 22:38:03 +0300

    Updated project. Added SaveNoteScreen.kt. Added fun ColorPicker(). Added Column(). Added Text(). Added LazyColumn(). Added ColorItem().

--- app/src/main/java/com/maksimyurau/android/lab5_notes/ui/SaveNoteScreen.kt
+++ app/src/main/java/com/maksimyurau/android/lab5_notes/ui/SaveNoteScreen.kt
@@ -1,20 +1,54 @@
 package com.maksimyurau.android.lab5_notes.ui
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.maksimyurau.android.lab5_notes.model.ColorModel
 import com.maksimyurau.android.lab5_notes.ui.components.NoteColor
 import com.maksimyurau.android.lab5_notes.util.fromHex
+
+@Composable
+private fun ColorPicker(
+    colors: List<ColorModel>,
+    onColorSelect: (ColorModel) -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = "Color picker",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(8.dp)
+        )
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            items(
+                colors.size
+            ) {
+                    itemIndex ->
+                val color = colors[itemIndex]
+                ColorItem(
+                    color = color,
+                    onColorSelect = onColorSelect
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun ColorItem(
