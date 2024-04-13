9fd3baaaea236cf3f403ecbc2821ad222ff92376
MaksimYurau <2004softwaredeveloper@gmail.com>
2024-04-13 22:28:31 +0300

    Updated project. Added SaveNoteScreen.kt. Added fun ColorItemPreview(). Added ColorItem().

--- app/src/main/java/com/maksimyurau/android/lab5_notes/ui/SaveNoteScreen.kt
+++ app/src/main/java/com/maksimyurau/android/lab5_notes/ui/SaveNoteScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.maksimyurau.android.lab5_notes.model.ColorModel
@@ -42,4 +43,10 @@ fun ColorItem(
                 .align(Alignment.CenterVertically)
         )
     }
+}
+
+@Preview
+@Composable
+fun ColorItemPreview() {
+    ColorItem(ColorModel.DEFAULT) {}
 }
