2e9b38a58d5f4b230df81b57488293f3a69a4f0d
MaksimYurau <2004softwaredeveloper@gmail.com>
2024-04-13 22:44:10 +0300

    Updated project. Added SaveNoteScreen.kt. Added fun ColorPickerPreview(). Added ColorPicker().

--- app/src/main/java/com/maksimyurau/android/lab5_notes/ui/SaveNoteScreen.kt
+++ app/src/main/java/com/maksimyurau/android/lab5_notes/ui/SaveNoteScreen.kt
@@ -84,3 +84,15 @@ fun ColorItem(
 fun ColorItemPreview() {
     ColorItem(ColorModel.DEFAULT) {}
 }
+
+@Preview
+@Composable
+fun ColorPickerPreview() {
+    ColorPicker(
+        colors = listOf(
+            ColorModel.DEFAULT,
+            ColorModel.DEFAULT,
+            ColorModel.DEFAULT
+        )
+    ) { }
+}
